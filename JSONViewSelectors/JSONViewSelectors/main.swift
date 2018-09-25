719dcdd063a872615775d5b70d7f3a2afaef0f92
Michael Friedman <frimicc@gmail.com>
2018-09-25 08:56:32 -0700

    Empty project files.

---
+++ JSONViewSelectors/JSONViewSelectors/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  JSONViewSelectors
+//
+//  Created by Michael Friedman on 9/25/18.
+//  Copyright © 2018 Michael Friedman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
